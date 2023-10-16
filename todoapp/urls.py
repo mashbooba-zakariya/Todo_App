8960bd2deed690aa0c93dfe1182e3f759d105323
mashbooba-zakariya <mashboobazakariya777@gmail.com>
2023-10-16 16:07:24 +0530

    third commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,5 +5,9 @@ from todoapp import views
 urlpatterns = [
     path('',views.index1,name='index1'),
     path('index2',views.index2,name='index2'),
-    path('create',views.create,name='create')
+    path('create',views.create,name='create'),
+    path('base',views.base,name='base'),
+    path('read',views.read,name='read'),
+    path('delete/<int:id>/',views.delete,name='delete')
+
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 
 from todoapp.forms import todoForm
+from todoapp.models import todo
 
 
 # Create your views here.
+
+def base(request):
+    return render(request,'base.html')
+
+
 def index1(request):
     return render(request,'index1.html')
 
@@ -21,3 +27,17 @@ def create(request):
             data.save()
             return redirect('/')
     return render(request,'index2.html',{'form':form})
+
+# read the todolist
+def read(request):
+    data = todo.objects.all()
+    print(data)
+    return render(request,'index4.html',{'data':data})
+
+def delete(request,id):
+    if request.method == 'POST':
+        data = todo.objects.get(id=id)
+        print(data)
+        data.delete()
+        return redirect('read')
+    
